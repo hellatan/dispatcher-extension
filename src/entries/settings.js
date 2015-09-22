2c442e9191385f132307a63b9fedcaad3236bd42
dale tan <wtdtan@gmail.com>
2015-09-22 01:58:57 -0400

    initial settings page setup

---
+++ src/entries/settings.js
@@ -0,0 +1,13 @@
+/**
+ * User: hella
+ * Date: 9/22/15
+ * Time: 1:51 AM
+ * Copyright 1stdibs.com, Inc. 2015. All Rights Reserved.
+ */
+
+'use strict';
+
+import React from 'react';
+import Settings from '../pages/settings.jsx';
+
+React.render(<Settings />, document.querySelector('#main'));
---
+++ src/pages/settings.jsx
@@ -0,0 +1,19 @@
+/**
+ * User: hella
+ * Date: 9/22/15
+ * Time: 12:25 AM
+ * Copyright 1stdibs.com, Inc. 2015. All Rights Reserved.
+ */
+
+'use strict';
+
+import React from 'react';
+
+import TextBox from '../components/TextBox/TextBox.jsx';
+import RadioGroup from '../components/RadioGroup/RadioGroup.jsx';
+
+export default class Settings extends React.Component {
+    render() {
+
+    }
+}
